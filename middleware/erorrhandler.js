e8f93f975284cee6cc2915e33f68d809b6c37dbe
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-06 03:19:20 +0500

    constants_Standard

--- middleware/erorrhandler.js
+++ middleware/erorrhandler.js
@@ -1,8 +1,18 @@
+const { contants } = require("../constants")
 const errorHandler = (err, request, response, next) => {
     const statusCode = response.statusCode ? response.statusCode : 500;
-    response.json({
-        message: err.message, stackTrace: err.stack
-    });
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            response.json({ title: "Validation Failed", message: err.message, stackTrace: err.stack });
+
+            break;
+        case 404:
+            response.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+
+            break;
+        default:
+            break;
+    }
 
 };
 
