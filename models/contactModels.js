18fd5c159e7c20c3ff0a8179314125ef7cd97556
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-07 03:37:37 +0500

    DB-models-added

---
+++ models/contactModels.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "please add the contact name "],
+    },
+    email: {
+        type: String,
+        required: [true, "please add the contact email "],
+    },
+    phone: {
+        type: String,
+        required: [true, "please add the contact Phone Number"],
+    },
+
+},
+    {
+        timestamps: true,
+    }
+)
+module.exports = mongoose.model("contacts", contactSchema)
