82ed9d626a078119ff2116b74d0c591a6f1454b0
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-07 02:20:32 +0500

    ErrorsAdded

--- constants.js
+++ constants.js
@@ -3,4 +3,5 @@ exports.constants = {
     UNAUTHORIZES: 401,
     FORBIDDEN: 403,
     NOT_FOUND: 404,
+    SERVER_ERROR: 500,
 }
--- middleware/erorrhandler.js
+++ middleware/erorrhandler.js
@@ -1,13 +1,21 @@
-const { contants } = require("../constants")
+const { constants } = require("../constants")
 const errorHandler = (err, request, response, next) => {
     const statusCode = response.statusCode ? response.statusCode : 500;
     switch (statusCode) {
         case constants.VALIDATION_ERROR:
             response.json({ title: "Validation Failed", message: err.message, stackTrace: err.stack });
-
             break;
-        case 404:
+        case constants.NOT_FOUND:
             response.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+            break
+        case constants.UNAUTHORIZES:
+            response.json({ title: "Un Authorized", message: err.message, stackTrace: err.stack });
+            break;
+        case constants.FORBIDDEN:
+            response.json({ title: "Forbidden", message: err.message, stackTrace: err.stack });
+            break;
+        case constants.SERVER_ERROR:
+            response.json({ title: "Server Error", message: err.message, stackTrace: err.stack });
 
             break;
         default:
