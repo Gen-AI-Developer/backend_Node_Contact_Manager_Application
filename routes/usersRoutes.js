56ceadc89c37017b421e5824809651875ba619f7
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-12 12:05:34 +0500

    update

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const router = express.router();
+const router = express.Router();
 router.post("/register", (request, response) => {
     response.json({ message: "Register the User" })
 });
